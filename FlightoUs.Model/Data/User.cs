0a66b9e5221249b581656c98d36d3581a32a75fe
Ahmad Tariq <ahmadtariq011@gmail.com>
2021-03-28 21:36:18 +0500

    com

---
+++ FlightoUs.Model/Data/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FlightoUs.Model.Data
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [StringLength(20)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Telephone { get; set; }
+
+        public int UserType { get; set; }
+
+        [StringLength(100)]
+        public string Company { get; set; }
+    }
+}
---
+++ FlightoUs.Model/Enums/UserRoleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightoUs.Model.Enums
+{
+    public enum UserRoleType
+    {
+        Admin = 1,
+        Manager=2,
+        User = 3
+    }
+}
