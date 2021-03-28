451a76887877e90b2bd2e6d4044d00ab42ce6f37
Ahmad Tariq <ahmadtariq011@gmail.com>
2021-03-28 20:39:06 +0500

    setup

---
+++ FlightoUs.Model/Filter/BaseFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LillyLifestyle.Models.Filters
+{
+    public class BaseFilter
+    {
+        public string Keyword { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public string Sort { get; set; }
+    }
+}
