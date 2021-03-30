dedd03396395600dd2b537189cd40e1f2880a11e
Muhammad Shameer <ranashameer700@gmail.com>
2021-03-30 21:04:39 +0500

    Created UserAPI methods in UserAPI-Controller

--- FlightoUs.Web/APIController/UserAPIController.cs
+++ FlightoUs.Web/APIController/UserAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using FlightoUs.Bll;
+using FlightoUs.Model.Data;
 using FlightoUs.Model.Services;
 using FlightoUs.Models.Filters;
 using Microsoft.AspNetCore.Mvc;
@@ -13,17 +14,86 @@ namespace FlightoUs.Web.APIController
     [Route("api/[controller]/[action]")]
     public class UserAPIController : Controller
     {
+
         BllUser bllUser = new BllUser();
-        private ServiceResponse result = new ServiceResponse { IsSucceeded = true };
+
+        ServiceResponse result = new ServiceResponse();
 
         [HttpPost]
-        public ServiceResponse GetUsersWithCount(UserSearchFilter filter)
+        public ServiceResponse SaveUser(User user)
         {
             try
             {
-                var lst = bllUser.Search(filter);
-                result.Message = lst;
-                result.TotalCount = bllUser.GetSearchCount(filter);
+                if (bllUser.GetByPK(user.Id) == null)
+                {
+                    User dbUser = new User();
+                    dbUser.FirstName = user.FirstName;
+                    dbUser.LastName = user.LastName;
+                    dbUser.UserName = user.UserName;
+                    dbUser.Email = user.Email;
+                    dbUser.Password = user.Password;
+                    dbUser.Telephone = user.Telephone;
+                    dbUser.CNIC = user.CNIC;
+                    dbUser.CreatedDate= DateTime.Now;
+                    dbUser.UserType = user.UserType;
+                    dbUser.GenderType = user.GenderType;
+
+
+                    int UserId = bllUser.Insert(dbUser);
+                    result.IsSucceeded = true;
+                    result.TotalCount = UserId;
+                }
+                else
+                {
+                    User dbUser = bllUser.GetByPK(user.Id);
+
+                    dbUser.FirstName = user.FirstName;
+                    dbUser.LastName = user.LastName;
+                    dbUser.Password = user.Password;
+                    dbUser.Telephone = user.Telephone;
+                    dbUser.CNIC = user.CNIC;
+                    dbUser.UserType = user.UserType;
+                    dbUser.GenderType = user.GenderType;
+
+
+                    bllUser.Update(dbUser);
+                    result.IsSucceeded = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                result.IsSucceeded = false;
+                result.Message = ex.Message+"<br>"+ex.StackTrace;
+            }
+            return result;
+        }
+
+        [HttpPost]
+        public ServiceResponse DeleteUser (int Id)
+        {
+            try
+            {
+
+                bllUser.DeleteUser(Id);
+                result.IsSucceeded = true;
+                result.Message = "User Deleted Successfully";
+            }
+
+            catch(Exception e)
+            {
+                result.IsSucceeded = false;
+                result.Message = e.Message + " </br> " + e.StackTrace;
+            }
+            return result;
+        }
+        
+        [HttpPost]
+        public ServiceResponse GetAllUsers()
+        {
+            try
+            {
+                result.Message = bllUser.GetAllUsers();
             }
             catch (Exception ex)
             {
@@ -34,19 +104,58 @@ namespace FlightoUs.Web.APIController
         }
 
         [HttpPost]
-        public ServiceResponse GetUsers(UserSearchFilter filter)
+
+        public ServiceResponse GetByEmail(string email)
         {
             try
             {
-                result.Message = bllUser.Search(filter);
+                result.Message = bllUser.GetByEmail(email);
             }
-            catch (Exception ex)
+            catch(Exception e)
             {
                 result.IsSucceeded = false;
-                result.Message = ex.Message + "<br>" + ex.StackTrace;
+                result.Message = e.Message + "<br>" + e.StackTrace;
             }
             return result;
         }
-
     }
 }
+
+
+
+/*
+BllUser bllUser = new BllUser();
+private ServiceResponse result = new ServiceResponse { IsSucceeded = true };
+
+[HttpPost]
+public ServiceResponse GetUsersWithCount(UserSearchFilter filter)
+{
+    try
+    {
+        var lst = bllUser.Search(filter);
+        result.Message = lst;
+        result.TotalCount = bllUser.GetSearchCount(filter);
+    }
+    catch (Exception ex)
+    {
+        result.IsSucceeded = false;
+        result.Message = ex.Message + "<br>" + ex.StackTrace;
+    }
+    return result;
+}
+
+[HttpPost]
+public ServiceResponse GetUsers(UserSearchFilter filter)
+{
+    try
+    {
+        result.Message = bllUser.Search(filter);
+    }
+    catch (Exception ex)
+    {
+        result.IsSucceeded = false;
+        result.Message = ex.Message + "<br>" + ex.StackTrace;
+    }
+    return result;
+}
+*/
